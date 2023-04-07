22ea73518a367fbfe27e4d16fe0aafae9cff5b44
Supriya Moond <ersupriya13@gmail.com>
2023-04-07 20:20:29 +0530

    Added backend config file

--- learn-terraform-netowrk/main.tf
+++ learn-terraform-netowrk/main.tf
@@ -6,6 +6,12 @@ terraform {
       version = "~> 3.0.2"
     }
   }
+  backend "azurerm" {
+      resource_group_name  = "tfstate"
+      storage_account_name = "tfstate13197"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
 
   required_version = ">= 1.1.0"
 }
