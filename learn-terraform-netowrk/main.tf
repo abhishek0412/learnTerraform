24a8ed57e3f0838d93581d31149f36da25b4ee29
Supriya Moond <ersupriya13@gmail.com>
2023-04-07 18:49:04 +0530

    Added my first terraform scripts

---
+++ learn-terraform-netowrk/main.tf
@@ -0,0 +1,29 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "myTerraformResource"
+  location = "eastus"
+}
+
+# Create a virtual network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myTerraformVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg.name
+}
